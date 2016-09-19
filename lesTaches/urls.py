0a2902efcbe0cfbb269a00613dd79ef4f202bab1
Theo Guerin <guerin@info27-11.iut45.univ-orleans.fr>
2016-09-19 15:52:02 +0200

    ué ué refacto

--- lesTaches/urls.py
+++ lesTaches/urls.py
@@ -18,5 +18,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^home/$',views.home),
+    url(r'^home/(\w)?$',views.home,name="home"),
 ]
--- lesTaches/views.py
+++ lesTaches/views.py
@@ -6,5 +6,7 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def home(request):
-    return HttpResponse("Hello")
+def home(request,name):
+    if not name:
+        name = "toi"
+    return HttpResponse("Hello Pd de "+name)
