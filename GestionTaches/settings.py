7c23c0a48741b823b4adb949b263a1a2c029d156
Theo Guerin <guerin@info27-11.iut45.univ-orleans.fr>
2016-10-03 15:49:33 +0200

    création des statics

--- GestionTaches/settings.py
+++ GestionTaches/settings.py
@@ -120,3 +120,7 @@ DATE_INPUT_FORMATS = ('%d/%m/%Y','%Y-%m-%d')
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
