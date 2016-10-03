5abe8088020216f0dbab3b6af4cab110fc11f73f
Theo Guerin <guerin@info27-11.iut45.univ-orleans.fr>
2016-10-03 15:42:37 +0200

    name unique

--- lesTaches/models.py
+++ lesTaches/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Task(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250,unique=True)
     description = models.TextField()
     createdDate = models.DateField(auto_now_add=True)
     closed = models.BooleanField(default=False)
