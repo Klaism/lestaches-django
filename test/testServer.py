4a2349c1f8d927098e8174936269817bfbd7c1b1
Théo Guerin <theo.guerin@nr-communication.fr>
2016-10-05 15:51:00 +0200

    refacto du test server

--- test/testServer.py
+++ test/testServer.py
@@ -1,15 +1,16 @@
+from django.test import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-browser = webdriver.Chrome()
+class ServerTest(TestCase):
 
-browser.get('http://localhost:8000/')
+    '''Test du serveur allumé et lesTaches dans le titre'''
+    def test_server_on(self):
 
-time.sleep(1)
-
-assert 'lesTaches' in browser.title
-
-time.sleep(1)
-
-browser.quit()
+        browser = webdriver.Chrome()
+        browser.get('http://localhost:8000/')
+        time.sleep(1)
+        assert 'lesTaches' in browser.title
+        time.sleep(1)
+        browser.quit()
